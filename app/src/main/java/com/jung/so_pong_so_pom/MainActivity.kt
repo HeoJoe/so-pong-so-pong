dd59cec3ad343b0b4d71d0395da6e64b1b41e276
jung0115 <jungim7490@gmail.com>
2023-07-10 18:08:27 +0900

    issue #1: layout 폴더 분류, 앱 아이콘 설정, viewBinding 세팅

--- app/src/main/java/com/jung/so_pong_so_pom/MainActivity.kt
+++ app/src/main/java/com/jung/so_pong_so_pom/MainActivity.kt
@@ -2,10 +2,21 @@ package com.jung.so_pong_so_pom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.jung.so_pong_so_pom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // ViewBinding Setting
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
