f68f83141c6769680ef645b33e93d43901040ebc
jung0115 <jungim7490@gmail.com>
2023-07-16 05:21:33 +0900

    issue #2: 스플래시 완

---
+++ app/src/main/java/com/jung/so_pong_so_pom/home/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.jung.so_pong_so_pom.home
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jung.so_pong_so_pom.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    // ViewBinding Setting
+    private var _binding: ActivityMainBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
